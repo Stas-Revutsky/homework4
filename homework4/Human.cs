a037f7389e97f47da886f66857143b5b1e52236c
Admin <stas.revutsky@gmail.com>
2023-05-31 23:10:08 +0300

    Add project files.

---
+++ homework4/Human.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4
+{
+    internal class Human
+    {
+        private string name;
+        private DateTime birthYear;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public DateTime BirthYear
+        {
+            get { return birthYear; }
+            set { birthYear = value; }
+        }
+
+        public Human()
+        {
+            name = "Unknown";
+            birthYear = DateTime.Now;
+        }
+
+        public Human(string name, DateTime birthYear)
+        {
+            Name = name;
+            this.birthYear = birthYear;
+        }
+
+        public int Age()
+        {
+            DateTime date0 = new DateTime(0001, 01, 01);
+            return ((date0 +(DateTime.Now - birthYear)).Year) - 1;
+        }
+
+        public static Human Input(int i)
+        {
+            Console.WriteLine($"Enter human {i + 1} name");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Enter human {i + 1} birthyear");
+            DateTime birthYear = DateTime.ParseExact(Console.ReadLine(), "yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None);
+            Human human = new Human(name, birthYear);
+            return human;
+        }
+
+        public void ChangeName(string newName)
+        {
+            name = newName;
+        }
+
+        public override string ToString()
+        {
+            return $"Hi, my name is {name} and my birthyear is {DateTime.Parse(birthYear.ToString()).Year} and i am {Age()} years old";
+        }
+
+        public void Output()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        public static bool operator == (Human a, Human b)
+        {
+            return a.name == b.name;
+        }
+
+        public static bool operator !=(Human a, Human b)
+        {
+            return a.name != b.name;
+        }
+    }
+}
---
+++ homework4/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace homework4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human[] humans = new Human[6];
+            for (int i = 0; i < humans.Length; i++)
+            {
+                humans[i] = Human.Input(i);
+            }
+            for (int i = 0; i < humans.Length; i++) 
+            {
+                humans[i].Output();
+            }
+            for(int i = 0;i < humans.Length; i++)
+            {
+                if (humans[i].Age() < 16)
+                {
+                    humans[i].ChangeName("Very young");
+                }
+            }
+            for (int i = 0; i < humans.Length; i++)
+            {
+                humans[i].Output();
+            }
+            for (int i = 0 ; i < humans.Length ; i++)
+            {
+                for (int j = i + 1; j < humans.Length; j++)
+                {
+                    if (humans[i] == humans[j])
+                    {
+                        Console.WriteLine($"{humans[i].Name} and {humans[j].Name} are equals");
+                    }
+                    else
+                    { Console.WriteLine($"Humans are not equals"); }
+                }
+            }
+        }
+    }
+}
